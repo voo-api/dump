8b72db12c702848f7babd0ebcb88ed3fe5cf50aa
vtrbtf <vitor.tfreitas@gmail.com>
2016-06-19 17:10:10 -0300

    Adding arrival flight

--- voo-dump.js
+++ voo-dump.js
@@ -19,16 +19,34 @@ if(!program.from && !program.to && !program['from-date'] && !program['to-date'])
   throw new Error('too few args. see --help')
 }
 
+source = {
+    name : "Decolar",
+    url : function(from, to, fromDate, toDate) {
+      return `http://www.decolar.com/shop/flights/results/multipleoneway/${from}/${to}/${fromDate}/${toDate}/1/0/0`
+    },
+    container : ".flights-cluster"
+}
+
+
 webdriverio
     .remote(options)
     .init()
-    .url(`http://www.decolar.com/shop/flights/results/multipleoneway/${program.from}/${program.to}/${program.fromDate}/${program.toDate}/1/0/0`)
+    .url(source.url(program.from, program.to, program.fromDate, program.toDate))
     .pause(20000)
-    .getHTML('.flights-cluster').then(function(err, html) {
+    .getHTML(source.container).then(function(err, html) {
         if (err) {
-          console.error({"err" : err , on : "to-flight", args : program.rawArgs});
+          console.error({"err" : err , on : "departure-flight", args : program.rawArgs});
         } else {
-          console.log({ "to-flight" : html });
+          console.log({ "departure-flight" : html });
+        }
+    })
+    .click('.arrival-item')
+    .pause(20000)
+    .getHTML(source.container).then(function(err, html) {
+        if (err) {
+          console.error({"err" : err , on : "arrival-flight", args : program.rawArgs});
+        } else {
+          console.log({ "arrival-flight" : html });
         }
     })
     .close()

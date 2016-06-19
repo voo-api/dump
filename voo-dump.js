e8922a9ef000e0c89954c7581473cbb42b8deb8b
vtrbtf <vitor.tfreitas@gmail.com>
2016-06-19 11:08:40 -0300

    Adding cli options

--- voo-dump.js
+++ voo-dump.js
@@ -1,4 +1,3 @@
-
 var webdriverio = require('webdriverio');
 var program = require('commander');
 
@@ -8,23 +7,28 @@ var options = {
     }
 };
 
-
 program
   .version('0.0.1')
-  .option('-f, --from', 'From')
-  .option('-t, --to', 'To')
+  .option('--from <cit>', 'From')
+  .option('--to <city>', 'To')
+  .option('--from-date <date>', 'From')
+  .option('--to-date <date>', 'To')
   .parse(process.argv);
 
+if(!program.from && !program.to && !program['from-date'] && !program['to-date']){
+  throw new Error('too few args. see --help')
+}
 
 webdriverio
     .remote(options)
     .init()
-    .url('http://www.decolar.com/shop/flights/results/multipleoneway/poa/rio/2016-06-24/2016-06-27/1/0/0')
+    .url(`http://www.decolar.com/shop/flights/results/multipleoneway/${program.from}/${program.to}/${program.fromDate}/${program.toDate}/1/0/0`)
     .pause(20000)
     .getHTML('.flights-cluster').then(function(err, html) {
         if (err) {
-          throw new Error(err)
+          console.error({"err" : err , on : "to-flight", args : program.rawArgs});
         } else {
-          console.log({ "to" : html })
+          console.log({ "to-flight" : html });
         }
     })
+    .close()

085777a56708d5928acd3e0b26be1511593f4aeb
scavusca <vitor.tfreitas@gmail.com>
2016-08-16 22:21:44 -0300

    Adding proxy based requests

--- scraper/local-proxies.js
+++ scraper/local-proxies.js
@@ -1,18 +1,35 @@
-const proxies = require('proxy-lists')
+const proxies = require('proxy-lists'),
+      Promise = require('bluebird'),
+      rp = require("request-promise");
 
+let proxyURIs = []
+let proxiesLoaded = false
 
-exports.loadproxies = function (cb) {
-	let proxiesArray = []
-	let alreadyReturned = false
-	proxies.getProxies({ countries: ['br'] })
+proxies.getProxies({ countries: ['br', 'us'] })
 	.on('data', (data) => {
-		proxiesArray = proxiesArray.concat(data)
+		proxyURIs = proxyURIs.concat(toProxyURI(data))
 	})
-	.once('end', () => {
-		if (!alreadyReturned) {
-			cb(proxiesArray.map((element) => {
-			  return "" + element.protocols[0] + '://' + element.ipAddress + ":" + element.port + "/" 
-			}))
-		}
+	.once('end', () => {	
+		proxiesLoaded = true		
 	})
+
+
+let proxiedReq = (url, proxy) => rp({ url : url, proxy: proxy, timeout: 5000 });
+let toProxyURI = (proxies) => {
+	proxies.map((element) => {
+		return "" + element.protocols[0] + '://' + element.ipAddress + ":" + element.port + "/" 
+	})
+}
+
+let hitEveryOne = (proxyURIs, url, cb, errcb) => {
+  Promise.some(proxyURIs.map((proxy) => proxiedReq(url, proxy)), 3)
+    .then(cb)
+    .catch(Promise.AggregateError, function(err) {
+		errcb(err)    		    	
+    });	
+}
+
+exports.toProxiedRequest = (url, cb, err) => {
+	while(!proxiesLoaded)
+	hitEveryOne(proxyURIs, url, cb, err)	  
 }
--- scraper/voo-dump.js
+++ scraper/voo-dump.js
@@ -1,48 +1,26 @@
 const localproxies = require('./local-proxies'),
-      Promise = require('bluebird'),
-      rp = require("request-promise")
-
-let options = {};
+      moment = require("moment");
 
 provider = {
     name : "Decolar",
-    url : (from, to, date) => `http://www.decolar.com/shop/flights/data/search/oneway/${from}/${to}/${date}/1/0/0/FARE/ASCENDING/NA/NA/NA/NA?itemType=SINGLETYPE&pageSize=1000&tripType=MULTIPLEONEWAY&resultsInstanceIndex=1`,   
+    destinations: ["poa", "rio"],
+    flights : () => {
+      let flightsAcc = []
+      for (var m = moment().add(60, 'days'); m.isAfter(moment()); m.subtract(1, 'days')) {
+        flightsAcc.push({from: provider.destinations[0], to: provider.destinations[1], date : m.format("YYYY-MM-DD")});
+        flightsAcc.push({from: provider.destinations[1], to: provider.destinations[0], date : m.format("YYYY-MM-DD")});
+      }
+      return flightsAcc
+    },
+    url : (flight) => `http://www.decolar.com/shop/flights/data/search/oneway/${flight.from}/${flight.to}/${flight.date}/1/0/0/FARE/ASCENDING/NA/NA/NA/NA?itemType=SINGLETYPE&pageSize=1000&tripType=MULTIPLEONEWAY&resultsInstanceIndex=1`,   
 }
 
-let proxiedReq = (url, proxy) => {
-  return rp({
-        url : url,
-        proxy: proxy,
-        timeout: 3000
-      })
-}
-
-localproxies.loadproxies((proxies) => {
-  let requests = proxies.map((elm) => proxiedReq(provider.url("poa", "rio" , "2016-10-11"), elm))
-  Promise.some(requests, 1)
-    .then(function (data) {
-      console.log({ flight: { provider : provider, "departure" : data }});    
-    })
-    .catch(Promise.AggregateError, function(err) {
-    });
+provider.flights().map(f => provider.url(f)).forEach(url => {
+  localproxies.toProxiedRequest(url, 
+  (data) => {
+    console.log({flight: { provider : provider.name, result : data }, url : url});
+  },
+  (err) => {
+    console.error({err: err});
+  })
 })
-
-// webdriverio    
-//     .remote(options)
-//     .init()
-//     .url(provider.url(program.from, program.to, program.fromDate, program.toDate))    
-//     .getHTML(provider.container).then((err, html) => {
-//         if (err) {
-//           console.error({"err" : err , on : "departure-flight", args : program.rawArgs});
-//         } else {
-//           console.log({ flight: { provider : provider, "departure" : html }});
-//         }
-//     })
-//     .click('.arrival-item')
-//     .getHTML(provider.container).then((err, html) => {
-//         if (err) {
-//           console.error({"err" : err , on : "arrival-flight", args : program.rawArgs});
-//         } else {
-//           console.log({ flight: { provider : provider, "arrival" : html }});
-//         }
-//     })

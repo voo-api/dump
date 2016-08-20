550b299168e25445ddaa8134d045852d680c286e
Vitor Barbosa de Freitas <vtrbtf@users.noreply.github.com>
2016-08-20 14:48:14 -0300

    proxied req (#1)
    * testing proxied reqs
    
    * Adding proxy based requests
    
    * Refactoring
    
    * WIP
    
    * WIP
    
    * Refactoring
    
    * Finishing with proxy stuff

---
+++ scraper/proxy/loader.js
@@ -0,0 +1,23 @@
+let proxies = require('proxy-lists');
+let proxyURIs = []
+
+let toProxyURI = (proxies) => {
+	return proxies.map((element) => "" + element.protocols[0] + '://' + element.ipAddress + ":" + element.port + "/" )
+}
+
+exports.load = (cb) => {
+	proxies.getProxies({anonymityLevels: ['elite'], countries: ['br']})
+	.on('data', (data) => {
+		console.log(`Loading more ${data.length} proxies ...`)			
+		proxyURIs = proxyURIs.concat(toProxyURI(data))
+	})	
+	.on('error', (data) => {
+		console.error(`Error loading more proxies ...`)
+		console.error(data)			
+	})
+	.once('end', () => {	
+		console.log(`Loaded ${proxyURIs.length} proxies`)				
+		proxyURIs.push(null)
+		cb(proxyURIs)			
+	})
+}
--- scraper/voo-dump.js
+++ scraper/voo-dump.js
@@ -1,43 +1,61 @@
-var webdriverio = require('webdriverio');
-var program = require('commander');
-
-
-let options = {};
-
-program
-  .version('0.0.1')
-  .option('--from <city>', 'From')
-  .option('--to <city>', 'To')
-  .option('--from-date <date>', 'From')
-  .option('--to-date <date>', 'To')
-  .parse(process.argv);
-
-if(!program.from && !program.to && !program['from-date'] && !program['to-date']){
-  throw new Error('too few args. see --help')
-}
+const request = require('./proxy/request'),
+      loader = require('./proxy/loader')
+      moment = require("moment");
 
 provider = {
     name : "Decolar",
-    url : (from, to, fromDate, toDate) => `http://www.decolar.com/shop/flights/results/multipleoneway/${from}/${to}/${fromDate}/${toDate}/1/0/0`,
-    container : ".flights-cluster"
+    destinations: ["poa", "rio", "fln", "bsb", "sao", "bhz"],
+    flights : () => {      
+      let flightsAcc = []
+      for (var m = moment().add(90, 'days'); m.isAfter(moment()); m.subtract(1, 'days')) {
+        provider.destinations.forEach((elm) => {
+          provider.destinations.forEach((sec_elm) => {
+            if (elm != sec_elm) {
+              flightsAcc.push({from: elm, to: sec_elm, date : m.format("YYYY-MM-DD")});      
+            }
+          })
+        })
+      }
+      console.log(`Getting info for ${flightsAcc.length} flights`)
+      return flightsAcc
+    },
+    url : (flight) => `http://www.decolar.com/shop/flights/data/search/oneway/${flight.from}/${flight.to}/${flight.date}/1/0/0/FARE/ASCENDING/NA/NA/NA/NA?itemType=SINGLETYPE&pageSize=1000&tripType=MULTIPLEONEWAY&resultsInstanceIndex=1`,   
 }
 
-webdriverio    
-    .remote(options)
-    .init()
-    .url(provider.url(program.from, program.to, program.fromDate, program.toDate))    
-    .getHTML(provider.container).then((err, html) => {
-        if (err) {
-          console.error({"err" : err , on : "departure-flight", args : program.rawArgs});
-        } else {
-          console.log({ flight: { provider : provider, "departure" : html }});
-        }
-    })
-    .click('.arrival-item')
-    .getHTML(provider.container).then((err, html) => {
-        if (err) {
-          console.error({"err" : err , on : "arrival-flight", args : program.rawArgs});
-        } else {
-          console.log({ flight: { provider : provider, "arrival" : html }});
-        }
-    })
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+function randomInt (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
+let lastScheduledCall = 0
+let sucessCounter = 0
+loader.load((proxyURIs) => {
+  shuffleArray(provider.flights())    
+    .forEach( (flight, index) => {
+      let scheduled = Math.floor((((index + 1)*(randomInt(2,100))) * 2000) / randomInt(20,100))
+      if (scheduled > lastScheduledCall) lastScheduledCall = scheduled;
+      setTimeout(() => {
+        request.toProxiedRequest(proxyURIs, provider.url(flight), 
+          (data) => {
+            data.forEach((elm) => {
+              if (elm.trim().startsWith("{\"result\":")) {
+                console.log({flight: { query: flight, provider : provider.name, result : data }, url : provider.url(flight), _meta : { time : new Date() }})
+              }              
+            })            
+          },
+          (err) => {
+            console.error({err: err.message});
+          })
+        }, scheduled)
+  })
+  console.log(`Last call scheduled to ${lastScheduledCall} msecs`)       
+})

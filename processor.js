a24592748a783ba6530f49dee6a894a6876b8661
vtrbtf <vitor.tfreitas@gmail.com>
2017-06-11 10:05:35 -0300

    processor

--- processor.js
+++ processor.js
@@ -6,7 +6,35 @@ var getDirectories = function (src, callback) {
 };
 
 var processPayload = function(data) {
-    return data;
+    var payload = []
+    data.result.data.items.forEach(item => {
+        var itineraries = item.itinerariesBox
+        console.log(Object.keys(itineraries.matchingInfoMap['_0_0']))
+        var price = {
+            base: itineraries.matchingInfoMap["_0_0"].itineraryTrackingInfo.emissionAdultPrice.amount,
+            total: itineraries.matchingInfoMap["_0_0"].itineraryTrackingInfo.price[0].amount
+        }
+        itineraries.outboundRoutes.forEach(itemOR => {
+            itineraries.inboundRoutes.forEach(itemIR => {
+                var outboundSegment =  itemOR.segments[0]
+                var inboundSegment =  itemIR.segments[0]
+                var flight = {
+                    provider: item.provider,
+                    price: price,
+                    outbound: {
+                        departure: outboundSegment.departure.hour,
+                        arrival: outboundSegment.arrival.hour
+                    },
+                    inbound: {
+                        departure: inboundSegment.departure.hour,
+                        arrival: inboundSegment.arrival.hour
+                    }
+                }
+                payload.push(flight)
+            })
+        })
+    })
+    return payload
 }
 
 getDirectories('data', function (err, res) {
